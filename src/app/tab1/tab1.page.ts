f136ffef9e9be2a8d06b7a8967ae3998c427a220
Ling Ma <lma1169949842@gmail.com>
2019-03-02 16:10:16 -0600

    addNewItem on 'Enter'
    Input new Item and add to the list

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -5,4 +5,32 @@ import { Component } from '@angular/core';
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {}
+export class Tab1Page {
+
+  item:any = {
+    name:'',
+    amount:'',
+    bought:false
+  }
+
+  items:array<item>;
+  constructor(){this.items=[]}
+
+  addNewItem(newitem:string){
+    let item:item={name:newitem,bought:false};
+    this.items.push(item);
+    this.newItem='';
+  }
+
+  bought($event,item:item){
+    item.bought=item.bought==false? true:false;
+    console.log(item.bought);
+  }
+
+  delete(item:item){
+    console.log('try to delete sth');
+    let delIndex=items.indexOf(item);
+    items.splice(delIndex,1);
+    console.log('probably didnt succeed.')
+  }
+}
